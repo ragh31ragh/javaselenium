d8c90b7731bc7881c062220fd00a44f78a8d7df2
ragh31ragh <ragh31ragh@gmail.com>
2025-02-27 13:04:00 +0530

    array explanation

--- src/coreJavaLearningtwofive/coreJavabasics.java
+++ src/coreJavaLearningtwofive/coreJavabasics.java
@@ -1,4 +1,5 @@
 //lecture 20 : About java data types 
+//lecture 21 : What are arrays in java ? How to initialize and retrieve the values of array 
 
 package coreJavaLearningtwofive;
 
@@ -14,6 +15,20 @@ public class coreJavabasics {
 		
 		System.out.println(num);
 		System.out.println(num + " My num is stored in num variable");
+		
+		//arrays
+		int[] intarray = new int[5];
+		int[] arr2 = {3,4,5,6,7};
+		intarray[0] = 1;
+		intarray[2] = 1;
+		intarray[3] = 1;
+		intarray[3] = 2;
+		intarray[4] = 1;
+		//intarray[5] = 1;
+		System.out.println(intarray[2]);
+		System.out.println(arr2[2]);
+		
+		
 
 	}
 
