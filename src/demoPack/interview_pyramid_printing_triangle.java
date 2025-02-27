03dab2056785668254ed907192cfabf42cf2ae59
ragh31ragh <ragh31ragh@gmail.com>
2025-02-27 20:09:17 +0530

    pyramid message

---
+++ src/demoPack/interview_pyramid_printing_triangle.java
@@ -0,0 +1,22 @@
+package demoPack;
+
+public class interview_pyramid_printing_triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k = 1 ;
+		for ( int i = 0; i < 4 ;i++) {
+			//System.out.println("Outer loop started ");
+
+			for (int j = 1 ; j <=  4-i ; j++ ) {
+				//System.out.println("Inner loop ");
+				System.out.print(k);
+				System.out.print("\t");
+				k++ ;
+		}
+			System.out.println("");
+		}
+
+	}
+
+}
---
+++ src/demoPack/pyramid_excercise2.java
@@ -0,0 +1,25 @@
+package demoPack;
+
+public class pyramid_excercise2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k = 1;
+		for (int i=1 ; i <=4 ;i++) {
+			for (int j = 1;j < i+1 ;j++) {
+				System.out.print(k);
+				System.out.print("\t");
+				k++;
+			}
+			
+			System.out.println("");
+		}
+
+	}
+
+}
+
+//1 
+//2 3 
+//4 5 6 
+//7 8 9 10
