d2a6660016d04dbe1141128a9e3284da308cfa27
ragh31ragh <ragh31ragh@gmail.com>
2025-02-27 12:41:21 +0530

    first file in src folder

---
+++ src/coreJavaLearningtwofive/coreJavabasics.java
@@ -0,0 +1,20 @@
+//lecture 20 : About java data types 
+
+package coreJavaLearningtwofive;
+
+public class coreJavabasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 5 ;
+		String website = "raghavendra test" ;
+		char letter = 'r';
+		double dec = 5.99 ;
+		boolean mycard = true ;
+		
+		System.out.println(num);
+		System.out.println(num + " My num is stored in num variable");
+
+	}
+
+}
