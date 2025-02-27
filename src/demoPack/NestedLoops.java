abe605a0d64cffa7d181d157862cf8b63deefb75
ragh31ragh <ragh31ragh@gmail.com>
2025-02-27 19:24:50 +0530

    while loops

---
+++ src/demoPack/NestedLoops.java
@@ -0,0 +1,26 @@
+package demoPack;
+
+public class NestedLoops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for ( int i = 1; i < 4 ;i++) {
+			//System.out.println("Outer loop started ");
+
+			for (int j = 1 ; j < 5 ; j++ ) {
+				//System.out.println("Inner loop ");
+				System.out.println(j);
+		}
+			//System.out.println("Outer loop ended ");
+		}
+			
+
+	}
+
+}
+
+//1234
+//567
+//89
+//10
