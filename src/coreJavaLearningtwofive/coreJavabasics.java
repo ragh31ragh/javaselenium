9c0bf484d310bc38b090338486e43fd67958a128
ragh31ragh <ragh31ragh@gmail.com>
2025-02-27 13:41:17 +0530

    string array learning

--- src/coreJavaLearningtwofive/coreJavabasics.java
+++ src/coreJavaLearningtwofive/coreJavabasics.java
@@ -20,14 +20,26 @@ public class coreJavabasics {
 		int[] intarray = new int[5];
 		int[] arr2 = {3,4,5,6,7};
 		intarray[0] = 1;
+		intarray[1] = 1;
 		intarray[2] = 1;
-		intarray[3] = 1;
 		intarray[3] = 2;
 		intarray[4] = 1;
 		//intarray[5] = 1;
 		System.out.println(intarray[2]);
 		System.out.println(arr2[2]);
 		
+		//for loop 
+		System.out.println("###for loop#####");
+		for ( int i = 0 ; i < intarray.length ; i++) {
+			System.out.println(intarray[i]);
+		}
+		
+		System.out.println("###strings for loop#####");
+		String[] names = {"rd","rahu","prashant"};
+		for (int j = 0 ; j < names.length ; j++) {
+			System.out.println(names[j]);
+			
+		}
 		
 
 	}
