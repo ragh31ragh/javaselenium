cd51bc003d60bc40e1d0295cc81c5a1c2b615c0c
ragh31ragh <ragh31ragh@gmail.com>
2025-02-28 14:46:37 +0530

    interface implementation

--- src/demoPack/AustralianTraffic.java
+++ src/demoPack/AustralianTraffic.java
@@ -1,28 +1,51 @@
 package demoPack;
 
-public class AustralianTraffic implements CentralTraffic{
+public class AustralianTraffic implements CentralTraffic,ContinentalTraffic{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CentralTraffic a = new AustralianTraffic();
+		a.redStop();
+		a.greenGo();
+		a.flashYellow();
+		AustralianTraffic b = new AustralianTraffic();
+		b.walkonsymbol();
+		ContinentalTraffic c = new AustralianTraffic();
+		c.TrainSymbol();
 
 	}
 
 	@Override
 	public void greenGo() {
 		// TODO Auto-generated method stub
+		System.out.println("green go implementation");
 		
 	}
 
+	public void walkonsymbol() {
+		// TODO Auto-generated method stub
+		System.out.println("walk on symbol");
+		
+	}
+	
 	@Override
 	public void redStop() {
 		// TODO Auto-generated method stub
+		System.out.println("red stop implementation");
 		
 	}
 
 	@Override
 	public void flashYellow() {
 		// TODO Auto-generated method stub
+		System.out.println("flash yellow implementation");
 		
+	}
+
+	@Override
+	public void TrainSymbol() {
+		// TODO Auto-generated method stub
+		System.out.println("Train stop implementation");
 	}
 
 }
