4f696fc7500af965633e240a0113d9c416d6932e
raghavendra d <ghanahamsa14@gmail.com>
2025-03-01 12:40:42 +0530

    about super keyword

---
+++ src/coreJavaLearningtwofive/ParentSuperDemo.java
@@ -0,0 +1,22 @@
+package coreJavaLearningtwofive;
+
+public class ParentSuperDemo {
+	String name = "Paul" ;
+	
+	public ParentSuperDemo() {
+		System.out.println("I am from parent class constructor");
+	}
+	
+	public void getData() {
+		System.out.println("I am from parent class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//String name = "Paul" ;
+
+	}
+
+}
---
+++ src/coreJavaLearningtwofive/childSuperDemo.java
@@ -0,0 +1,30 @@
+package coreJavaLearningtwofive;
+
+public class childSuperDemo extends ParentSuperDemo {
+	String name = "QA click";
+	
+	public childSuperDemo(){
+		super();
+		System.out.println("child class constructor");
+	}
+	
+	public void getStringData() {
+		System.out.println(name);
+		System.out.println(super.name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		childSuperDemo cd = new childSuperDemo();
+		cd.getStringData();
+		cd.getData();
+
+	}
+	
+	public void getData() {
+		super.getData();
+		System.out.println("I am from child class");
+	}
+
+}
